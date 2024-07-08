a7d26b440794cb2f720b6e88c612e31f41cb68a4
Cesarf17 <62317932+Cesarf17@users.noreply.github.com>
2024-07-08 18:12:01 -0500

    Test for push

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Import the screens for the Home and Settings
+// Import the screens for the Home, Settings, and SignIn
 import HomeScreen from './components/HomeScreen';
 import SettingsScreen from './components/SettingsScreen';
+import SignInScreen from './components/SignInScreen';
 
 // Create a Stack Navigator
 const Stack = createStackNavigator();
@@ -15,8 +16,10 @@ export default function App() {
   return (
     // Wrap the navigator inside NavigationContainer to manage the navigation tree
     <NavigationContainer>
-      {/* Define the stack navigator and set the initial route to Home */}
-      <Stack.Navigator initialRouteName="Home">
+      {/* Define the stack navigator and set the initial route to SignIn */}
+      <Stack.Navigator initialRouteName="SignIn">
+        {/* Define the SignIn screen route */}
+        <Stack.Screen name="SignIn" component={SignInScreen} />
         {/* Define the Home screen route */}
         <Stack.Screen name="Home" component={HomeScreen} />
         {/* Define the Settings screen route */}
