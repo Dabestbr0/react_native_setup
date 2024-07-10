5624f8b5ac32576f4f55cff747ce1e490e6fddb5
Cesarf17 <62317932+Cesarf17@users.noreply.github.com>
2024-07-10 16:51:42 -0500

    Fixed the log in and added logo and some background colors

--- App.js
+++ App.js
@@ -2,31 +2,33 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
-//import HomeScreen from './screens/HomeScreen';
-import HomeContent from './screens/HomeContent'; // Import your HomeContent screen
-import RunTimerStartScreen from './screens/RunTimerScreen'; // Import your RunTimerStartScreen
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SettingsProvider } from './contexts/SettingsData'; // Assuming SettingsProvider is for context
-import Ionicons from 'react-native-vector-icons/Ionicons'
-
-import ProfilePage from './screens/ProfilePage';
-import SettingsScreen from './screens/SettingsScreen';
-
-const Tab = createBottomTabNavigator();
+import HomeContent from './screens/HomeContent';
+import RunTimerStartScreen from './screens/RunTimerScreen';
+import { SettingsProvider } from './contexts/SettingsData';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SettingsProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="HomeContent" component={HomeContent} />
-          <Stack.Screen name="RunTimerStart" component={RunTimerStartScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="HomeContent" component={HomeContent} />
+            <Stack.Screen name="RunTimerStart" component={RunTimerStartScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
     </SettingsProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6', // Light blue background color
+  },
+});
